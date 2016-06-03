521f66f343205331960cff53ee9717b7fa42c1b8
kochie94 <kochie_94@hotmail.com>
2016-06-03 13:49:26 +1000

    data is string

--- main.py
+++ main.py
@@ -60,10 +60,10 @@ if __name__ == '__main__':
         }
     }
     bin_id = 2
-    payload = {'points':[
+    payload = {'points': [
         {
             'bin_id': bin_id,
-            'data': data,
+            'data': str(data),
             'lat': lat,
             'lng': lng,
             'time': time_now
